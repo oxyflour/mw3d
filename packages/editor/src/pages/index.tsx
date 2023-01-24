675062364585e264b79d6f1a6f8dc442373c195f
oxyflour <oxyflour@hotmail.com>
2023-01-24 23:44:15 +0800

    remove sender and receiver

--- packages/editor/src/pages/index.tsx
+++ packages/editor/src/pages/index.tsx
@@ -1,27 +1,13 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Sender, Receiver } from '@ttk/react'
 
 import './index.less'
-
-const peerOpts = {
-    iceServers: [{
-        urls: 'turn:172.31.60.216',
-        username: 'any',
-        credential: 'any',
-        credentialType: 'password'
-    }, {
-        urls: 'stun:172.31.60.216',
-    }]
-} as RTCConfiguration
 
 export function layout({ children }: { children: any }) {
     const [, sess = ''] = location.pathname.match(/\/sess\/(\w+)/) || [],
         nav = useNavigate()
     useEffect(() => { sess || nav(`/sess/${Math.random().toString(16).slice(2, 10)}`) }, [sess])
-    return navigator.gpu ?
-        <Sender peerOpts={ peerOpts }>{ children }</Sender> :
-        <Receiver peerOpts={ peerOpts } channel={ sess } />
+    return children
 }
 
 export function loading() {
