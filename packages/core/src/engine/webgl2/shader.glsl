b8ef3925138c4611c27479d7b77943d132584702
oxyflour <oxyflour@hotmail.com>
2023-02-12 12:27:15 +0800

    add define from threejs

--- packages/core/src/engine/webgl2/shader.glsl
+++ packages/core/src/engine/webgl2/shader.glsl
@@ -1,6 +1,13 @@
 // @chunk:common
 // @vert
 #version 300 es
+
+// https://github.com/mrdoob/three.js/blob/master/src/renderers/webgl/WebGLProgram.js
+#define attribute in
+#define varying out
+#define texture2D texture
+precision mediump sampler2DArray;
+
 uniform vec2 renderCanvasSize;
 uniform int renderLightNum;
 uniform vec4 renderLight0;
@@ -18,16 +25,22 @@ uniform vec4 materialColor;
 uniform vec4 materialProp;
 uniform vec4 materialClip;
 
-in vec3 position;
+attribute vec3 position;
 void main() {
     gl_Position = cameraViewProjection * meshModelMatrix * vec4(position, 1.0);
 }
 // @frag
 #version 300 es
+#define gl_FragColor out_fragColor
+#define gl_FragDepthEXT gl_FragDepth
+#define texture2D texture
 precision mediump float;
+
+// https://github.com/mrdoob/three.js/blob/master/src/renderers/webgl/WebGLProgram.js
+layout(location = 0) out highp vec4 out_fragColor;
+
 uniform vec4 materialColor;
 
-out vec4 out_FragColor;
 void main() {
-    out_FragColor = vec4(materialColor.rgb, 1.);
+    gl_FragColor = vec4(materialColor.rgb, 1.);
 }
